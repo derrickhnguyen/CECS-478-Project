1430d395b2893272cce86cbd38ddc51f05c5a7ff
Derrick Nguyen <Derrick Nguyen>
2017-02-08 13:37:10 -0800

    Changed syntax.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const http = require('http');
-
 const port = process.env.PORT || 5000;
 const host = process.env.HOST || 'localhost';
 
 http.createServer((req, res) => {
 
-	res.writeHead(200, {
-		'Content-Type': 'text/html; charset=utf-8',
-	});
+  res.writeHead(200, {
+    'Content-Type': 'text/html; charset=utf-8',
+  });
 
-	res.write('<h1>Mining for Goldstein!</h1>');
-	res.end();
+  res.write('<h1>Mining for Goldstein!</h1>');
+  res.end();
+
 }).listen(port, host);
 
 console.log(`App running at http://${host}:${port}`);
