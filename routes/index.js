5f45dfcef9b9a45ce68e6ab9cf6e7409888087f3
Derrick Nguyen <Derrick Nguyen>
2017-02-19 13:22:54 -0800

    Created encryptor method

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var encryptor = require('../backend/crypto');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  encryptor("my message", "backend/keys/public/public-key-1");
   res.render('index', { title: 'Mining for Goldstein' });
 });
 
