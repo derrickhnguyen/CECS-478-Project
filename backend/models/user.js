5a74c6968d047dc4c3756758901d2b7f273a3232
Derrick Nguyen <Derrick Nguyen>
2017-04-15 15:11:57 -0700

    Changed scrypt back to bcrypt to due many errors.

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,31 +1,40 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const scrypt = require('scrypt')
+const bcrypt = require('bcrypt-nodejs')
 
 // Define our model
 const userSchema = new Schema({
   firstname : { type: String },
   lastname  : { type: String },
   email     : { type: String, unique: true, lowercase: true },
-  password  : Buffer
+  password  : String
 })
 
 // On Save Hook, encrypt password
 // Before saving a model, run this function
 userSchema.pre('save', function(next) {
   const user = this
-  scrypt.kdf(user.password, {N: 1, r: 1, p: 1}, function(err, result) {
+
+  // Generate a salt then run callback
+  bcrypt.genSalt(10, function(err, salt) {
     if (err) return next(err)
-    user.password = result
-    next()
+
+    // Hash (encrypt) password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err)
+
+      // Overwrite plaint text password with encrypted password
+      user.password = hash
+      next()
+    })
   })
 })
 
 userSchema.methods.comparePassword = function(candidatePassword, callback) {
-  const user = this
-  scrypt.verifyKdf(user.password, new Buffer(candidatePassword), function(err, result) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
     if (err) return callback(err)
-    callback(null, result)
+
+    callback(null, isMatch)
   })
 }
 
